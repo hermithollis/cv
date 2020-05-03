3a60187c17d8513e4dfeed7239882ea54838c08c
Hollis <hollisvallotton@reps2.com>
2020-05-03 12:11:52 -0700

    Added ripple effect to navigation

--- hollis-vallotton/src/components/Link.vue
+++ hollis-vallotton/src/components/Link.vue
@@ -1,24 +1,42 @@
 <template>
-  <router-link v-bind:class="{ tab: true, hover }" v-bind:to="url">{{ name }}</router-link>
+  <div v-on:click="outerBoxClicked(url)">
+    <ripple>
+      <router-link v-bind:class="{ tab: true, hover }" v-bind:to="url">{{ name }}</router-link>
+    </ripple>
+  </div>
 </template>
 
 <script>
+import Ripple from '@/components/Ripple'
+import router from '@/router'
 export default {
   data () {
     return {
       hover: false
     }
   },
+  components: {
+    ripple: Ripple
+  },
   props: ['name', 'url'],
   methods: {
     mouseOver: function () {
       this.hover = !this.hover
+    },
+    outerBoxClicked: function (url) {
+      const { hash } = window.location
+      if (!hash.includes(url)) router.push(url)
     }
   }
 }
 </script>
 
 <style scoped>
+div {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
 .tab {
   padding: 5px;
   color:white;
